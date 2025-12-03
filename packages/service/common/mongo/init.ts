8bc43301a6da19d643d5398a97e429f9c4bf541f
dingzhenznen <1324652616@qq.com>
2025-12-03 14:20:07 +0800

    fix66

--- packages/service/common/mongo/init.ts
+++ packages/service/common/mongo/init.ts
@@ -53,19 +53,21 @@ export async function connectMongo(props: {
       } catch (error) {}
     });
 
-    await db.connect(url, {
+    const options = {
       bufferCommands: true,
-      maxConnecting: maxConnecting, // 最大连接数: 防止连接数过多时无法满足需求
-      maxPoolSize: maxConnecting, // 最大连接池大小: 防止连接池过大时无法满足需求
-      minPoolSize: 20, // 最小连接数: 20,防止连接数过少时无法满足需求
-      connectTimeoutMS: 60000, // 连接超时: 60秒,防止连接失败时长时间阻塞
-      waitQueueTimeoutMS: 60000, // 等待队列超时: 60秒,防止等待队列长时间阻塞
-      socketTimeoutMS: 60000, // Socket 超时: 60秒,防止Socket连接失败时长时间阻塞
-      maxIdleTimeMS: 300000, // 空闲连接超时: 5分钟,防止空闲连接长时间占用资源
-      retryWrites: true, // 重试写入: 重试写入失败的操作
-      retryReads: true, // 重试读取: 重试读取失败的操作
-      serverSelectionTimeoutMS: 10000 // 服务器选择超时: 10秒,防止副本集故障时长时间阻塞
-    });
+      maxConnecting: maxConnecting,
+      maxPoolSize: maxConnecting,
+      minPoolSize: 20,
+      connectTimeoutMS: 60000,
+      waitQueueTimeoutMS: 60000,
+      socketTimeoutMS: 60000,
+      maxIdleTimeMS: 300000,
+      retryWrites: true,
+      retryReads: true,
+      serverSelectionTimeoutMS: 10000
+    };
+
+    await db.connect(url, options);
     console.log('mongo connected22');
 
     connectedCb?.();
